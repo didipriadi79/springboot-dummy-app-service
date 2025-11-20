0218d402633f80f7ee3580f5891b2c171d709360
didipriadi79 <didipriadi79@gmail.com>
2025-11-21 00:16:51 +0700

    update getalluser hardcode

--- src/main/java/com/priadi/dummyapp/controllers/HomeController.java
+++ src/main/java/com/priadi/dummyapp/controllers/HomeController.java
@@ -2,6 +2,7 @@ package com.priadi.dummyapp.controllers;
 
 import com.priadi.dummyapp.domain.BaseResponse;
 import com.priadi.dummyapp.domain.dto.LoginDTO;
+import com.priadi.dummyapp.domain.dto.UserDTO;
 import com.priadi.dummyapp.domain.request.LoginRequest;
 import com.priadi.dummyapp.services.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class HomeController {
@@ -31,6 +35,30 @@ public class HomeController {
     public BaseResponse<LoginDTO> loginOnline(@RequestBody LoginRequest request){
         BaseResponse<LoginDTO> response = new BaseResponse<>();
         response = authService.loginOnline(request);
+        return response;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/get-all-user")
+    public BaseResponse<List<UserDTO>> getAllUser(){
+        BaseResponse<List<UserDTO>> response = new BaseResponse<>();
+
+        List<UserDTO> listUser = new ArrayList<>();
+        UserDTO user1 = new UserDTO();
+        user1.setId(1L);
+        user1.setName("didi");
+        user1.setEmail("didi@gmail.com");
+        user1.setUsername("didi79");
+
+        UserDTO user2 = new UserDTO();
+        user2.setId(2L);
+        user2.setName("didi");
+        user2.setEmail("didi@gmail.com");
+        user2.setUsername("didi79");
+
+        listUser.add(user1);
+        listUser.add(user2);
+        
+        response.setData(listUser);
         return response;
     }
 }
